a673ab466a5859d2c883500c2db52309f2603008
Wenxiao Zeng <wzeng@umass.edu>
2022-11-09 08:55:28 -0500

    basic structure

--- server.js
+++ server.js
@@ -7,6 +7,34 @@ var server = http.createServer(function(request, response) {
     var path = url.parse(request.url).pathname; 
     switch (path){
         // gluing all html pages
+        case '/':  
+            response.writeHead(200, {  
+                'Content-Type': 'text/plain'  
+            });  
+            response.write("This is Test Message.");  
+            response.end();  
+            break;
+        case '/login':
+            fs.readFile('./EnumerationMachine_LoginPage.html',function(error,data){
+                if (error) {  
+                    response.writeHead(404);  
+                    response.write(error);  
+                    response.end();  
+                } else {  
+                    response.writeHead(200, {  
+                        'Content-Type': 'text/html'  
+                    });  
+                    response.write(data);  
+                    response.end();  
+                }
+            });
+            break;
+        
+        default:  
+            response.writeHead(404);  
+            response.write("Page does not exist.");  
+            response.end();  
+            break;
     }
 });
 
