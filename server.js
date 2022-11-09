7867cf8ef3fc36438da48847301846d3d9a28c04
Wenxiao Zeng <wzeng@umass.edu>
2022-11-09 08:41:18 -0500

    basic server structure

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 //javascript file for server functions
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+var server = http.createServer(function(request, response) {
+    var path = url.parse(request.url).pathname; 
+    switch (path){
+        // gluing all html pages
+    }
+});
+
+server.listen(8080);
