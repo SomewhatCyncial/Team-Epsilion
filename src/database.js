811b9a59a981e8d07e511fee76b73a4cf73c6ac5
Bryan Kazunas <bkazunas@umass.edu>
2022-11-09 17:05:04 -0500

    created and connected to mongo cluster

--- src/database.js
+++ src/database.js
@@ -1,1 +1,23 @@
 //javascript file for database functions
+const {MongoClient} = require('mongodb');
+
+
+
+async function main() {
+
+    const uri = "mongodb+srv://TeamEpsilon:qeRbSxbrBo1icaxl@cluster0.3kmj7nn.mongodb.net/?retryWrites=true&w=majority";
+
+    const client = new MongoClient(uri);
+    try{
+        await client.connect();
+    }
+    catch(err){
+        console.error(err);
+    }
+    finally{
+        await client.close();
+    }
+
+}
+
+main().catch(console.error);
